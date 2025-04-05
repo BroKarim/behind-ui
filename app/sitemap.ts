80b54e4e2728131ca0c9aeaacf6e6b868f517a4f
dzulkiram hilmi <bukutulis0110@gmail.com>
2025-04-05 14:17:00 +0700

    chore: replace sitemap.ts with static version after removing content-collections

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,3 @@
-import { allDocs, allPages } from "content-collections";
 import { MetadataRoute } from "next";
 import { headers } from "next/headers";
 
@@ -12,13 +11,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${protocol}://${domain}`,
       lastModified: new Date(),
     },
-    ...allPages.map((post) => ({
-      url: `${protocol}://${domain}/${post.slugAsParams}`,
-      lastModified: new Date(),
-    })),
-    ...allDocs.map((post) => ({
-      url: `${protocol}://${domain}/docs/${post.slugAsParams}`,
-      lastModified: post.date,
-    })),
   ];
 }
